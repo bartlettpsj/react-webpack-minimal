55753a96e5f128a9ce516c7ffebe8c24d095b0e7
Paul Bartlett <paul.bartlett@walmartlabs.com>
2019-02-25 22:47:08 -0800

    Add uglify

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const webpack = require('webpack');
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
 
 module.exports = {
    mode: 'production',    
@@ -15,7 +16,10 @@ module.exports = {
       inject: 'body'
     }),
     new webpack.HotModuleReplacementPlugin()    
-   ],  
+   ],
+   optimization: {
+    minimizer: [new UglifyJsPlugin({ sourceMap: true })]
+  },
    devtool: 'inline-source-map', //'#source-map',   
    devServer: {
     contentBase: path.resolve(__dirname, 'dist'),
